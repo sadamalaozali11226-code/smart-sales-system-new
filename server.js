1f466cad14dc871509cdba76653e2f27a1078eef
sadamalaozali11226-code <sadamalaozali11226@gmail.com>
2026-09-26 22:43:45 +0300

    Change products declaration from const to let

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(__dirname));
 
-const products = [];
+let products = [];
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
